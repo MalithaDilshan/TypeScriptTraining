3355771dde8d944bc162c89d8936bdd6975e3547
root <root@LAPTOP-8E7PCJBI.localdomain>
2021-12-10 18:26:14 +0530

    First type script training with a call

---
+++ main.js
@@ -0,0 +1,45 @@
+// Interface declaration 
+// interface Point
+// {
+//     x : number,
+//     y : number,
+//     draw : () => void
+// }
+class Point {
+    // '?' take this as optional ( TypeScript can not have multiple contractors )
+    constructor(x, y) {
+        this._x = x;
+        this._y = y;
+    }
+    // constructor( private x? : number, private y? : number )
+    // {
+    // }
+    draw() {
+        console.log("Coordinate - X : " + this._x + "  Y : " + this._y);
+    }
+    getX() {
+        return this._x;
+    }
+    setX(value) {
+        if (value < 0) {
+            throw new Error(" Value is not in the valid range");
+        }
+        this._x = value;
+    }
+    // Declare as a property access modifier 
+    get y() {
+        return this._y;
+    }
+    set y(value) {
+        if (value < 0) {
+            throw new Error(" Value is not in the valid range");
+        }
+        this._y = value;
+    }
+}
+// let point = new Point ();
+let point1 = new Point(4, -1);
+// point.x = 4;
+// point.y = 5;
+point1.draw();
+console.log(" Y: ", point1.y);
